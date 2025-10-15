da7bb496df7511a45b57238bc55c2a53bf689bad
Retr0IsNotAvailable <josephmagodks15846@gmail.com>
2025-10-16 01:58:04 +0200

    make weather-icon and weather interactive

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,68 @@
 const apiKey = 'c3e548f6786068b9807ffb978f6d1e24';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?' +
-  'units=metric&q=germany';
+  'units=metric';
+
+const inputElement = document.querySelector('.js-input');
+const buttonElement = document.querySelector('.js-button');
+const weatherIconElement = document.querySelector('.js-weather-icon');
   
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city = 'madrid') {
+  const response = await fetch(apiUrl + `&appid=${apiKey}` + `&q=${city}`);
   var data = await response.json();
 
-  console.log(data);
+  document.querySelector('.js-city')
+    .innerHTML = data.name;
+  
+  document.querySelector('.js-temp')
+    .innerHTML = `${Math.round(data.main.temp)}&#176;C`;
+  
+  document.querySelector('.js-humidity')
+    .innerHTML = `${data.main.humidity}%`;
+  
+  document.querySelector('.js-wind')
+    .innerHTML = `${Math.round(data.wind.speed)} km/h`;
+  
+  switch (data.weather[0].main) {
+    case 'Clouds':
+      weatherIconElement.src = './assets/icons/clouds.png';
+      break;
+    case 'Clear':
+      weatherIconElement.src = './assets/icons/clear.png';
+      break;
+    case 'Rain':
+      weatherIconElement.src = './assets/icons/rain.png';
+      break;
+    case 'Drizzle':
+      weatherIconElement.src = './assets/icons/drizzle.png';
+      break;
+    case 'Mist':
+      weatherIconElement.src = './assets/icons/mist.png';
+      break;
+    default:
+      break;
+  }
+  document.querySelector('.weather')
+    .style.display = 'block';
 }
 
-checkWeather();
+function searchCity() {
+  if (inputElement.value.trim()) {
+    const city = inputElement.value.trim();
+    inputElement.value = '';
+
+    checkWeather(city);
+  } else {
+    alert('U must enter a city name!');
+    inputElement.value = '';
+  }
+}
+
+buttonElement.addEventListener('click', () => {
+  searchCity();
+});
+
+inputElement.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    searchCity();
+  }
+});
