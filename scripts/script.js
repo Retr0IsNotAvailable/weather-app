8735903666c1423dc9badb0d569838b2d6b7e0fa
Retr0IsNotAvailable <josephmagodks15846@gmail.com>
2025-10-16 02:15:21 +0200

    add status 404 error handling

--- scripts/script.js
+++ scripts/script.js
@@ -5,44 +5,53 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?' +
 const inputElement = document.querySelector('.js-input');
 const buttonElement = document.querySelector('.js-button');
 const weatherIconElement = document.querySelector('.js-weather-icon');
+const weatherElement = document.querySelector('.weather');
+const errorElement = document.querySelector('.error');
   
 async function checkWeather(city = 'madrid') {
   const response = await fetch(apiUrl + `&appid=${apiKey}` + `&q=${city}`);
-  var data = await response.json();
+  if (response.status === 404) {
+    errorElement.style.display = 'block';
 
-  document.querySelector('.js-city')
-    .innerHTML = data.name;
-  
-  document.querySelector('.js-temp')
-    .innerHTML = `${Math.round(data.main.temp)}&#176;C`;
-  
-  document.querySelector('.js-humidity')
-    .innerHTML = `${data.main.humidity}%`;
-  
-  document.querySelector('.js-wind')
-    .innerHTML = `${Math.round(data.wind.speed)} km/h`;
-  
-  switch (data.weather[0].main) {
-    case 'Clouds':
-      weatherIconElement.src = './assets/icons/clouds.png';
-      break;
-    case 'Clear':
-      weatherIconElement.src = './assets/icons/clear.png';
-      break;
-    case 'Rain':
-      weatherIconElement.src = './assets/icons/rain.png';
-      break;
-    case 'Drizzle':
-      weatherIconElement.src = './assets/icons/drizzle.png';
-      break;
-    case 'Mist':
-      weatherIconElement.src = './assets/icons/mist.png';
-      break;
-    default:
-      break;
+    weatherElement.style.display = 'none';
+  } else {
+    errorElement.style.display = 'none';
+
+    var data = await response.json();
+
+    document.querySelector('.js-city')
+      .innerHTML = data.name;
+    
+    document.querySelector('.js-temp')
+      .innerHTML = `${Math.round(data.main.temp)}&#176;C`;
+    
+    document.querySelector('.js-humidity')
+      .innerHTML = `${data.main.humidity}%`;
+    
+    document.querySelector('.js-wind')
+      .innerHTML = `${Math.round(data.wind.speed)} km/h`;
+    
+    switch (data.weather[0].main) {
+      case 'Clouds':
+        weatherIconElement.src = './assets/icons/clouds.png';
+        break;
+      case 'Clear':
+        weatherIconElement.src = './assets/icons/clear.png';
+        break;
+      case 'Rain':
+        weatherIconElement.src = './assets/icons/rain.png';
+        break;
+      case 'Drizzle':
+        weatherIconElement.src = './assets/icons/drizzle.png';
+        break;
+      case 'Mist':
+        weatherIconElement.src = './assets/icons/mist.png';
+        break;
+      default:
+        break;
+    }
+    weatherElement.style.display = 'block';
   }
-  document.querySelector('.weather')
-    .style.display = 'block';
 }
 
 function searchCity() {
